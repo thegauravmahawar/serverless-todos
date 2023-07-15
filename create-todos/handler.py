c54fb41061f3308bd4bd893ab3d2b6a913ce5d4f
Gaurav Mahawar <thegauravmahawar@gmail.com>
2023-07-15 21:21:42 +0530

    Update serverless handlers

--- create-todos/handler.py
+++ create-todos/handler.py
@@ -1,7 +1,7 @@
 import json
 
 
-def hello(event, context):
+def create_todos(event, context):
     body = {
         "message": "Go Serverless v3.0! Your function executed successfully!",
         "input": event,
