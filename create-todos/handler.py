d367555ba3d031518b182c5ae5601e1a2dfc0fd2
Gaurav Mahawar <thegauravmahawar@gmail.com>
2023-07-16 05:59:02 +0530

    Update handlers

--- create-todos/handler.py
+++ create-todos/handler.py
@@ -1,5 +1,4 @@
 import json
-import boto3
 
 def create(event, context):
 
--- get-todos/handler.py
+++ get-todos/handler.py
@@ -6,7 +6,7 @@ def get(event, context):
     id = event['pathParameters']['id']
     payload = {
         'task': 'Run',
-        'description': 'Run 5 Kms',
+        'description': 'Run 10 Kms',
         'priority': 3,
         'date': '15-07-2023'
     }
